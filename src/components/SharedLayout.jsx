3df0f0998e9c91c76b843138a28171ebbd7dc995
Yevhenii Pozniak <jekamanu@gmail.com>
2023-11-22 14:17:05 +0200

    update

---
+++ src/components/SharedLayout.jsx
@@ -0,0 +1,23 @@
+import { Link, Outlet } from 'react-router-dom';
+import { Container, Header, Logo } from './App.styled';
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+        <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{' '}
+          GoMerch Store
+        </Logo>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+      </Header>
+      <Outlet />
+    </Container>
+  );
+};
